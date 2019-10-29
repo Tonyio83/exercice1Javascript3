da5449cfa8e1040670830f539b4c0f101ca87aa1
Tonyio83 <anthonyfrancelle@gmail.com>
2019-10-29 13:49:16 +0100

    exercice 1 correction

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let image = document.getElementById('image1');
+image.onmouseover = function(){
+  image.style.border = '3px solid red';
+}
+image.onmouseout = function(){
+  image.style.border = 'none'
+}
